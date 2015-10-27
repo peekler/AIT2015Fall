9abaa837d8f9bf61d50a24d65a7e234b9c90fce3
Peter Ekler <peter.ekler@aut.bme.hu>
2015-10-27 11:29:47 +0100

    Add animation added

--- TodoApp/app/src/main/java/hu/ait/android/todoapp/adapter/TodoRecylerAdapter.java
+++ TodoApp/app/src/main/java/hu/ait/android/todoapp/adapter/TodoRecylerAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +26,7 @@ public class TodoRecylerAdapter extends
 
     private List<Todo> todos;
     private Context context;
+    private int lastPosition = -1;
 
     public TodoRecylerAdapter(Context context) {
         this.context = context;
@@ -54,6 +57,8 @@ public class TodoRecylerAdapter extends
                         "Todo clicked "+position, Toast.LENGTH_SHORT).show();
             }
         });
+
+        setAnimation(holder.itemView, position);
     }
 
     @Override
@@ -83,6 +88,17 @@ public class TodoRecylerAdapter extends
             }
         }
         notifyItemMoved(oldPosition, newPosition);
+    }
+
+    private void setAnimation(View viewToAnimate, int position)
+    {
+        // If the bound view wasn't previously displayed on screen, it's animated
+        if (position > lastPosition)
+        {
+            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
--- TodoApp/app/src/main/java/hu/ait/android/todoapp/touch/TodoListTouchHelper.java
+++ TodoApp/app/src/main/java/hu/ait/android/todoapp/touch/TodoListTouchHelper.java
@@ -20,6 +20,7 @@ public class TodoListTouchHelper extends ItemTouchHelper.Callback {
     public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
         int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+
         return makeMovementFlags(dragFlags, swipeFlags);
     }
 
